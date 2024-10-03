e6b66eb704c5896444430dca1eafcc26c84beb99
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-10-03 13:29:10 +0200

    Add a guard clause to prevent pokémon duplication

--- src/comps/ModalContent.tsx
+++ src/comps/ModalContent.tsx
@@ -27,6 +27,14 @@ export function ModalContent({
       // Generation I (Kanto): 151 Pokémon (Pokémon #001-#151)
       // Introduced in Pokémon Red/Blue/Green/Yellow (1996)
       const randomId = Math.floor(Math.random() * 151) + 1;
+
+      // Check for duplication using the .some() method
+      const duplicationFound = pokemon.some((p) => p.id === randomId);
+      if (duplicationFound) {
+        console.log(`Duplication Found - Skipping Pokémon! ID: ${randomId}`);
+        continue; // Skip to the next iteration if duplication is found
+      }
+
       const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${randomId}`
       );
