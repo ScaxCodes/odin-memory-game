2bf474410b8e6fd1f1167edcf505771615060764
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-10-05 14:57:36 +0200

    Add react-howler library and background-music

--- src/comps/ToggleMusicButton.tsx
+++ src/comps/ToggleMusicButton.tsx
@@ -1,5 +1,7 @@
 import musicOn from "../assets/music.png";
 import musicOff from "../assets/music-off.png";
+import ReactHowler from "react-howler";
+import bgMusic from "../assets/bgmusic.mp3";
 
 type ToggleMusicButtonType = {
   isMusicEnabled: boolean;
@@ -11,11 +13,17 @@ export function ToggleMusicButton({
   setIsMusicEnabled,
 }: ToggleMusicButtonType) {
   return (
-    <button
-      className="fixed top-0 left-0 m-4"
-      onClick={() => setIsMusicEnabled(!isMusicEnabled)}
-    >
-      <img src={isMusicEnabled ? musicOn : musicOff} />
-    </button>
+    <>
+      <button
+        className="fixed top-0 left-0 m-4"
+        onClick={() => setIsMusicEnabled(!isMusicEnabled)}
+      >
+        <img src={isMusicEnabled ? musicOn : musicOff} />
+      </button>
+      {/* {isMusicEnabled && <ReactHowler src="./assets/bgmusic.mp3" />} */}
+      {isMusicEnabled && (
+        <ReactHowler src={bgMusic} playing={true} loop={true} volume={0.2} />
+      )}
+    </>
   );
 }
